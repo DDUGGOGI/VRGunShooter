c9ee999d24f96f14952e45f70bd2f07e934d433a
DDUGGOGI <95662494+DDUGGOGI@users.noreply.github.com>
2022-04-14 17:26:43 +0900

    CC Setup

--- Assets/Scripts/NetworkLocalPlayer.cs
+++ Assets/Scripts/NetworkLocalPlayer.cs
@@ -16,9 +16,28 @@ public class NetworkLocalPlayer : MonoBehaviourPunCallbacks
     Vector3 pos;
     public float speed;
 
+
+
+
+    //CC컴포넌트
+    public CharacterController cc;
+
+    //중력 가속도의 크기
+    public float gravity = -20;
+    //수직 속도
+    float yVelocity = 0;
+    //점프 힘
+    public float jumpPower = 2;
+
     void Start()
     {
         pos = transform.position;
+        if (PV.IsMine)
+        {
+            cc = GetComponent<CharacterController>();
+
+        }
+        
     }
 
     void Update()
@@ -26,9 +45,11 @@ public class NetworkLocalPlayer : MonoBehaviourPunCallbacks
         if (!PV.IsMine)
         {
             Destroy(ovrCamRig);
+
         }
         else
         {
+
             // 플레이어 카메라 관리
             if (centerCam.tag != "MainCamera")
             {
@@ -43,25 +64,8 @@ public class NetworkLocalPlayer : MonoBehaviourPunCallbacks
             leftHand.localPosition = InputTracking.GetLocalPosition(Node.LeftHand);
             rightHand.localPosition = InputTracking.GetLocalPosition(Node.RightHand);
 
-            // 왼쪽 조이스틱 엄지스틱 위치 관리
-            Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-            if (primaryAxis.y > 0f)
-            {
-                pos += (primaryAxis.y * transform.forward * Time.deltaTime * speed);
-            }
-            if (primaryAxis.y <0f)
-            {
-                pos += (Mathf.Abs(primaryAxis.y) * -transform.forward * Time.deltaTime * speed);
-            }
-            if (primaryAxis.x > 0f)
-            {
-                pos += (primaryAxis.y * transform.right * Time.deltaTime * speed);
-            }
-            if (primaryAxis.x < 0f)
-            {
-                pos += (Mathf.Abs(primaryAxis.y) * -transform.right * Time.deltaTime * speed);
-            }
-            transform.position = pos;
+            PlayerArrowMove();
+            //youtubesMove();
 
             // 오른쪽 조이스틱 엄지스틱 회전 관리
             Vector3 euler = transform.rotation.eulerAngles;
@@ -70,5 +74,57 @@ public class NetworkLocalPlayer : MonoBehaviourPunCallbacks
             transform.rotation=Quaternion.Euler(euler);
             transform.localRotation = Quaternion.Euler(euler);
         }
+    }
+
+    void youtubesMove()
+    {
+        // 왼쪽 조이스틱 엄지스틱 위치 관리
+        Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        if (primaryAxis.y > 0f)
+        {
+            pos += (primaryAxis.y * transform.forward * Time.deltaTime * speed);
+        }
+        if (primaryAxis.y < 0f)
+        {
+            pos += (Mathf.Abs(primaryAxis.y) * -transform.forward * Time.deltaTime * speed);
+        }
+        if (primaryAxis.x > 0f)
+        {
+            pos += (primaryAxis.y * transform.right * Time.deltaTime * speed);
+        }
+        if (primaryAxis.x < 0f)
+        {
+            pos += (Mathf.Abs(primaryAxis.y) * -transform.right * Time.deltaTime * speed);
+        }
+        transform.position = pos;
+    }
+    void PlayerArrowMove()
+    {
+        //사용자의 입력을 받는다.
+        float h = ARAVRInput.GetAxis("Horizontal", ARAVRInput.Controller.LTouch);
+        float v = ARAVRInput.GetAxis("Vertical", ARAVRInput.Controller.LTouch);
+        //방향을 만든다.
+        Vector3 dir = new Vector3(h, 0, v);
+        //사용자가 보는 방향 dir
+        dir = Camera.main.transform.TransformDirection(dir);
+        //중력을 적용한 수직방향 추가
+        yVelocity += gravity * Time.deltaTime;
+        if (cc.isGrounded)
+        {
+            yVelocity = 0;
+        }
+        if (ARAVRInput.GetDown(ARAVRInput.Button.Two, ARAVRInput.Controller.RTouch) && cc.isGrounded)
+        {
+            yVelocity = jumpPower;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && cc.isGrounded)
+        {
+            yVelocity = jumpPower;
+        }
+        dir.y = yVelocity;
+
+        //이동한다.
+        cc.Move(dir * speed * Time.deltaTime);
     }
 }
